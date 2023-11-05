d91f941e186b7dc52b4478512a0fcad48b98710d
Bunea Gabriel <buneagabriel@Buneas-MacBook-Pro.local>
2023-11-05 11:49:48 +0200

    Revert "frontent design & expenses"
    This reverts commit a70ef98111abd9438ec8a6096d51ea1bfc8af301.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,8 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import DrawerNav from './components/DrawerNav'
-import Home from './pages/Home'
+
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<DrawerNav />}>
-                    <Route index element={<Home />}></Route>
-                </Route>
-            </Routes>
-        </Router>
+        <div>App</div>
     )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,4 @@ import { render } from "react-dom";
 import React from "react";
 
 const appDiv = document.getElementById("app");
-render(
-    <div className="app">
-        <App />
-    </div>
-    , appDiv);
+render(<App />, appDiv);
